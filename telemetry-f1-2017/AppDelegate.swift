3359821b23c67161eedc5f453718593326f91ed0
Mitchell Holland <mitchell@mitchellholland.net>
2018-05-04 10:37:08 +1000

    Initial Commit

---
+++ telemetry-f1-2017/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  telemetry-f1-2017
+//
+//  Created by Mitchell Holland on 4/5/18.
+//  Copyright © 2018 Mitchell Holland. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ telemetry-f1-2017/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  telemetry-f1-2017
+//
+//  Created by Mitchell Holland on 4/5/18.
+//  Copyright © 2018 Mitchell Holland. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
