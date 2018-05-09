6041c9feaf767a947c4f29aca5f54c9b81b1bc10
Mitchell Holland <mitchell@mitchellholland.net>
2018-05-09 12:13:03 +1000

    No longer blocks main thread getting data
    - Centered label in window
    - Port can be set as launch argument
    - Added gitignore

--- telemetry-f1-2017/ViewController.swift
+++ telemetry-f1-2017/ViewController.swift
@@ -10,15 +10,35 @@ import AppKit
 
 class ViewController: NSViewController {
     
-    @IBOutlet weak var speedLabel: NSTextField!
+    @IBOutlet private weak var speedLabel: NSTextField!
     
-    let refreshRate = 60.0
+    let communicationQueue = DispatchQueue(label: "MXHCommunicationQueue", qos: .userInteractive)
     
-    lazy var timer: Timer = {
-        return Timer(timeInterval: TimeInterval(exactly: 1.0/refreshRate)!, target: self, selector: #selector(go), userInfo: nil, repeats: true)
-    }()
+    private let refreshRate = 60.0
+    private var port: Int {
+        let defaultPort = 20777
+        let defaults = UserDefaults.standard
+        
+        guard let udpPortString = defaults.string(forKey: "UDPPort") else {
+            return defaultPort
+        }
+        guard let udpPort = Int(udpPortString) else {
+            return defaultPort
+        }
+        return udpPort
+    }
     
-    var udp: UDPPacket? {
+    private var timer: Timer?
+    private func startTimer() {
+        communicationQueue.async {
+            self.timer = Timer(timeInterval: 1.0/self.refreshRate, target: self, selector: #selector(self.updateData), userInfo: nil, repeats: true)
+            let runLoop = RunLoop.current
+            runLoop.add(self.timer!, forMode: .commonModes)
+            runLoop.run()
+        }
+    }
+    
+    private var udp: UDPPacket? {
         didSet {
             guard let packet = udp else {
                 return
@@ -28,20 +48,21 @@ class ViewController: NSViewController {
         }
     }
     
+    private let startNotification = Notification.init(name: Notification.Name(rawValue: "MXHStartNotification"))
+    
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        NotificationCenter.default.addObserver(self, selector: #selector(go), name: Notification.Name(rawValue: "MXHGo"), object: nil)
-        DispatchQueue.global().async {
-            NotificationCenter.default.post(Notification.init(name: Notification.Name(rawValue: "MXHGo")))
-            
-            RunLoop.main.add(self.timer, forMode: .commonModes)
-        }
-        
         speedLabel.font = NSFont.monospacedDigitSystemFont(ofSize: 42, weight: .regular)
+        setLabel(value: "0")
+        NotificationCenter.default.addObserver(self, selector: #selector(start), name: startNotification.name, object: nil)
+        NotificationCenter.default.post(startNotification)
     }
     
-    let formatter: MeasurementFormatter = {
+    @objc private func start() {
+        startTimer()
+    }
+    
+    private let formatter: MeasurementFormatter = {
         let formatter = MeasurementFormatter()
         formatter.unitStyle = .short
         let numberFormatter = NumberFormatter()
@@ -50,14 +71,15 @@ class ViewController: NSViewController {
         return formatter
     }()
     
-    @objc func go() {
+    @objc private func updateData() {
         let socket = boundSocket
+        // dont read into global `udp`, but read into `packet` then replace contents
         var packet = UDPPacket()
         read(fromSocket: socket, into: &packet)
         udp = packet
     }
     
-    func setLabel(value: String) {
+    private func setLabel(value: String) {
         DispatchQueue.main.async {
             self.speedLabel.stringValue = value
         }
@@ -65,9 +87,7 @@ class ViewController: NSViewController {
     
     private var address_len = socklen_t(MemoryLayout<sockaddr_in>.stride)
     
-    lazy var boundSocket: Int32 = {
-        let port = 20777
-        
+    private lazy var boundSocket: Int32 = {
         let sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)
         guard sock != -1 else {
             fatalError("error creating socket: \(sock)")
@@ -79,7 +99,7 @@ class ViewController: NSViewController {
         address.sin_addr = in_addr(s_addr: INADDR_ANY)
         
         /// `int bind(int socket, const struct sockaddr *address, socklen_t address_len);`
-        /// use `withUnsafePointer` because pointer of address is needed
+        /// use `withUnsafePointer` because old APIs are great
         withUnsafePointer(to: &address) {
             
             /// cast `sockaddr_in` -> `sockaddr` with `withMemoryRebound`
@@ -91,13 +111,13 @@ class ViewController: NSViewController {
         return sock
     }()
     
-    func read(fromSocket socket: Int32, into packet: UnsafeMutablePointer<UDPPacket>!) {
+    private func read(fromSocket socket: Int32, into packet: UnsafeMutablePointer<UDPPacket>!) {
         var address = sockaddr()
         recvfrom(socket, packet, 2048, 0, &address, &address_len)
     }
     
     //    https://gist.github.com/neonichu/c504267a23ca3f3126bb
-    func htons(value: CUnsignedShort) -> CUnsignedShort {
+    private func htons(value: CUnsignedShort) -> CUnsignedShort {
         return (value << 8) + (value >> 8)
     }
 }
